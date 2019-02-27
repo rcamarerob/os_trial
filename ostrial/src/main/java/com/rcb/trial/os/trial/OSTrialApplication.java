969d17c0a390a04c2e66f81cf1288d50bf71f8b8
Raul Camarero <raul.camarero@fon.com>
2019-02-27 17:13:35 +0100

    Empty application

---
+++ ostrial/src/main/java/com/rcb/trial/os/trial/OSTrialApplication.java
@@ -0,0 +1,14 @@
+package com.rcb.trial.os.trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OSTrialApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OSTrialApplication.class, args);
+    }
+}
